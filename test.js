e5ac638bff1cd9a06f2e5adbd2129726bf422582
anonymous98-source <rugveddeshpande18@gmail.com>
2025-11-04 12:51:36 +0530

    Update test.js

--- test.js
+++ test.js
@@ -14,60 +14,61 @@ import java.util.stream.Collectors;
 @Service
 public class MenuService {
 
-    public MenuResponse transform(List<Permissions> permissionsList) {
-        MenuResponse response = new MenuResponse();
+	public MenuResponse transform(List<Permissions> permissionsList) {
+		MenuResponse response = new MenuResponse();
 
-        // Group by root menuTitle
-        Map<String, List<Permissions>> grouped = permissionsList.stream()
-                .collect(Collectors.groupingBy(Permissions::getMenuTitle));
+		// Group by root menuTitle
+		Map<String, List<Permissions>> grouped = permissionsList.stream()
+				.collect(Collectors.groupingBy(Permissions::getMenuTitle));
 
-        List<RootMenu> rootMenus = new ArrayList<>();
+		List<RootMenu> rootMenus = new ArrayList<>();
 
-        for (Map.Entry<String, List<Permissions>> entry : grouped.entrySet()) {
-            List<Permissions> group = entry.getValue();
-            Permissions rootPerm = group.get(0);
+		for (Map.Entry<String, List<Permissions>> entry : grouped.entrySet()) {
+			List<Permissions> group = entry.getValue();
+			Permissions rootPerm = group.get(0);
 
-            RootMenu rootMenu = new RootMenu();
-            rootMenu.setId(rootPerm.getMenuId());
-            rootMenu.setTitle(rootPerm.getMenuTitle());
-            rootMenu.setIcon(rootPerm.getMenuIcon());
-            rootMenu.setRoute(rootPerm.getMenuUrl());
-            rootMenu.setComponentPath(rootPerm.getComponentPath());
-            rootMenu.setScreenDescription(rootPerm.getMenuDescription());
+			RootMenu rootMenu = new RootMenu();
+			rootMenu.setId(rootPerm.getMenuId());
+			rootMenu.setTitle(rootPerm.getMenuTitle());
+			rootMenu.setIcon(rootPerm.getMenuIcon());
+			rootMenu.setRoute(rootPerm.getMenuUrl());
+			rootMenu.setComponentPath(rootPerm.getComponentPath());
+			rootMenu.setScreenDescription(rootPerm.getMenuDescription());
+			rootMenu.setOrderId(rootPerm.getOrderId());
 
+			if (group.size() > 1) {
+				rootMenu.setHasChildren(true);
 
-            if (group.size() > 1) {
-                rootMenu.setHasChildren(true);
+				List<ChildMenu> children = new ArrayList<>();
+				for (Permissions p : group) {
+					if (p.getSubMenu() != null) {
+						ChildMenu child = getChildMenu(p);
+						children.add(child);
+					}
+				}
+				rootMenu.setChildren(children);
+			} else {
+				rootMenu.setHasChildren(false);
+			}
 
-                List<ChildMenu> children = new ArrayList<>();
-                for (Permissions p : group) {
-                    if (p.getSubMenu() != null) {
-                        ChildMenu child = getChildMenu(p);
-                        children.add(child);
-                    }
-                }
-                rootMenu.setChildren(children);
-            } else {
-                rootMenu.setHasChildren(false);
-            }
+			rootMenus.add(rootMenu);
+		}
 
-            rootMenus.add(rootMenu);
-        }
+		response.setRoot_menus(rootMenus);
+		return response;
+	}
 
-        response.setRoot_menus(rootMenus);
-        return response;
-    }
+	@NotNull
+	private static ChildMenu getChildMenu(Permissions p) {
+		ChildMenu child = new ChildMenu();
+		child.setId(p.getMenuId());
+		child.setTitle(p.getSubMenu());
+		child.setIcon(p.getMenuIcon());
+		child.setRoute(p.getMenuUrl());
+		child.setComponentPath(p.getComponentPath());
+		child.setScreenDescription(p.getMenuDescription());
+		child.setOrderId(p.getOrderId());
 
-    @NotNull
-    private static ChildMenu getChildMenu(Permissions p) {
-        ChildMenu child = new ChildMenu();
-        child.setId(p.getMenuId());
-        child.setTitle(p.getSubMenu());
-        child.setIcon(p.getMenuIcon());
-        child.setRoute(p.getMenuUrl());
-        child.setComponentPath(p.getComponentPath());
-        child.setScreenDescription(p.getMenuDescription());
-        
-        return child;
-    }
-} 	
+		return child;
+	}
+}
