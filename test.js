e9f4f7e01564bc558499493e6bb12c81a4d39718
anonymous98-source <rugveddeshpande18@gmail.com>
2025-11-10 16:10:37 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,30 +1,15 @@
-package com.fincore.gateway.Config;
+package com.fincore.gateway.dto;
 
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-/**
- * Utility to parse a PEM RSA public key string into RSAPublicKey
- */
-public class KeyUtils {
-
-    public static RSAPublicKey parseRsaPublicKey(String pem) {
-        try {
-            String clean = pem
-                    .replace("-----BEGIN PUBLIC KEY-----", "")
-                    .replace("-----END PUBLIC KEY-----", "")
-                    .replaceAll("\\s", "");
-            byte[] decoded = Base64.getDecoder().decode(clean);
-            X509EncodedKeySpec spec = new X509EncodedKeySpec(decoded);
-            KeyFactory kf = KeyFactory.getInstance("RSA");
-            return (RSAPublicKey) kf.generatePublic(spec);
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            throw new IllegalArgumentException("Invalid RSA public key", e);
-        }
-    }
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScreenDetails {
+    private String screenId;
+    private String screenTitle;
+    private String componentPath;
+    private String description;
 }
-
