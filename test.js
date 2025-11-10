0628b258eb8a823dff086e3a7ec35712e4432f04
anonymous98-source <rugveddeshpande18@gmail.com>
2025-11-10 15:55:57 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,60 +1,35 @@
 package com.fincore.gateway.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-/**
- * Entity representing permission details for each menu or action in the system.
- * 
- * Maps to the PERMISSIONS table and defines information such as menu title,
- * icon, sub-menu, related action, and route path used within the application.
- */
+import java.sql.Timestamp;
+
 @Entity
-@Data
-@Table(name = "PERMISSIONS")
-public class Permissions {
-
-    /** Unique ID for the menu or permission entry. */
+@Getter
+@Setter
+@Table(name = "USER_SESSION")
+public class RefreshToken {
     @Id
-    @Column(name = "MENU_ID")
-    private int menuId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SESSION_ID")
+    private Long sessionId;
 
-    /** Title displayed for this menu item or feature. */
-    @Column(name = "MENU_TITLE")
-    private String menuTitle;
+    @Column(name = "USER_ID")
+    private String userid;
 
-    /** Icon associated with the menu item (used in UI). */
-    @Column(name = "MENU_ICON")
-    private String menuIcon;
+    @Column(name = "REFRESH_TOKEN")
+    private String refreshToken;
 
-    /** Name of the parent or sub menu this item belongs to. */
-    @Column(name = "MENU_SUBMENU")
-    private String subMenu;
+    @Column(name = "SESSION_STARTED_AT", insertable = false, updatable = false)
+    private Timestamp sessionStartedAt;
 
-    /** Specific action type or permission (like VIEW, EDIT, DELETE etc). */
-    @Column(name = "MENU_ACTION")
-    private String menuAction;
+    @Column(name = "SESSION_ENDED_AT")
+    private Timestamp sessionEndedAt;
 
-    /** URL path or endpoint linked with this menu. */
-    @Column(name = "MENU_URL")
-    private String menuUrl;
+    @Column(name = "DEVICE_ID")
+    private String deviceId;
 
-    /** Component path or frontend route used for navigation. */
-    @Column(name = "MENU_COMPONENT_PATH")
-    private String componentPath;
 
-    /** Short description explaining what this permission or menu does. */
-    @Column(name = "MENU_DESCRIPTION")
-    private String menuDescription;
-
-    /** Indicates if this menu depends on another one. */
-    @Column(name = "MENU_DEPENDANT")
-    private String dependent;
-
-    /** Order in which the menu appears in UI. */
-    @Column(name = "MENU_ORDER")
-    private int orderId;
 }
