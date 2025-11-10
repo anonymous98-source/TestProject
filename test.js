95fba3a1ff885748833861e2ca712923f274ad27
anonymous98-source <rugveddeshpande18@gmail.com>
2025-11-10 15:59:21 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,22 +3,23 @@ package com.fincore.gateway.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-
-
+/**
+ * Entity representing the mapping between roles and permissions.
+ * 
+ * Each entry defines which permission belongs to which role.
+ * This acts as a bridge table between USER_ROLES and PERMISSIONS.
+ */
 @Entity
 @Data
 @Table(name = "ROLE_PERMISSIONS")
 public class RolePermissions {
 
-
-    /*ROLE_ID, PERMISSION_ID*/
-
+    /** ID of the role. */
     @Id
     @Column(name = "ROLE_ID")
-    int roleId;
+    private int roleId;
 
+    /** ID of the permission linked to the role. */
     @Column(name = "PERMISSION_ID")
-    int permissionId;
-
-
+    private int permissionId;
 }
